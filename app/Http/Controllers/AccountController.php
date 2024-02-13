e091601a62592868c87d6e46d6e47b37e9c70dbf
pokhrelkabir2075@gmail.com <pokhrelkabir2075@gmail.com>
2024-02-13 23:26:37 +0545

    User registration are finished properly by usin ajax

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AccountController extends Controller
+{
+    //This method show the user registration page
+    public function registration()
+    {
+        return view('front.account.registration');
+    }
+    //This method will save a user
+    public function processRegistration(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|same:confirm_password',
+            'confirm_password' => 'required'
+        ]);
+
+        if ($validator->passes()) {
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            session()->flash('success', 'You have registered successfully');
+
+            return response()->json([
+                'status' => true,
+                'errors' => []
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+    }
+
+    //This method show the user Login page
+    public function login()
+    {
+        return view('front.account.login');
+    }
+
+    public function authencate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->passes()) {
+            //Matchin the database value and user entered value
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                //if database value and user entered value is mathched the return to the profile page
+                return redirect()->route('account.profile');
+
+            } else {
+
+                return redirect()->route('account.login')->with('error', 'Invalid email or password');
+            }
+        } else {
+            return redirect()
+                ->route('account.login')
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
+        }
+    }
+
+    public function profile()
+    {
+        //Finding the user id from database
+        $id = Auth::user()->id;
+        //dd($id);
+        //Fetchin the all the details of user
+        $user = User::where('id', $id)->first();
+
+        return view('front.account.profile', [
+            'user' => $user
+
+        ]);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $id = Auth::user()->id;
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:10|max:50',
+            'email' => 'required|email|unique:users,email,' . $id . ',id'
+
+        ]);
+
+        if ($validator->passes()) {
+
+            $user = User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->mobile = $request->mobile;
+            $user->designation = $request->designation;
+            $user->save();
+
+            session()->flash('success', 'Profile updated successfully');
+
+            return response()->json([
+                'status' => true,
+                'error' => [],
+            ]);
+
+        } else {
+            return response()->json([
+                'status' => false,
+                'error' => $validator->errors(),
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('account.login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+
+
+Route::group(['prefix' =>'account'], function () {
+
+    //Guest Route
+    Route::group(['middleware' => 'guest'], function () {
+
+        Route::get('/register', [AccountController::class, 'registration'])->name('account.registration');
+
+        Route::post('/process-register', [AccountController::class, 'processRegistration'])->name('account.processRegistration');
+
+        Route::get('/login', [AccountController::class, 'login'])->name('account.login');
+
+        Route::post('/authencate', [AccountController::class, 'authencate'])->name('account.authencate');
+
+    });
+
+    //Authencated Routes
+
+    Route::group(['middleware' => 'auth'], function () {
+
+        Route::get('/profile', [AccountController::class, 'profile'])->name('account.profile');
+
+        Route::put('/update-profile', [AccountController::class, 'updateProfile'])->name('account.updateProfile');
+
+        Route::get('/logout', [AccountController::class, 'logout'])->name('account.logout');
+
+    });
+
+});
