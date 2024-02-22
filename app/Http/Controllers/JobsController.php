36647760b42c53807c8e6b0772bb9c04307a9fb9
pokhrelkabir2075@gmail.com <pokhrelkabir2075@gmail.com>
2024-02-22 23:00:43 +0545

    Latest job displayed in a home page and working in a find job where user can find the jobs on the basis of filter

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\JobType;
+use Illuminate\Http\Request;
+
+class JobsController extends Controller
+{
+    //This method will show thw jobs page
+    public function index()
+    {
+
+        $categories = Category::where('status', 1)->get();
+        $jobTypes = JobType::where('status', 1)->get();
+        $jobs = Job::where('status', 1)->with('jobType')->orderBy('created_at', 'DESc')->paginate(9);
+
+        return view('front.jobs', [
+
+            'categories' => $categories,
+            'jobTypes' => $jobTypes,
+            'jobs' => $jobs,
+        ]);
+    }
+}
